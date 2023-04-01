dcf21a58a342d1ee038804101720ea7b29f29e17
David Zeman <david.zeman@outlook.com>
2023-04-01 13:56:23 +0200

    models

---
+++ CityPlanner/CityPlanner/Models/Map.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityPlanner.Models
+{
+    internal class Map
+    {
+        public DemographicUnit[,] Matrix { get; set; }
+    }
+}
