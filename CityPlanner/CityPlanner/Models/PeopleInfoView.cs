1be6dff0db7627ea8e664e53dcc0c6a5417ddd03
kwimbik <kwimbiks@gmail.com>
2023-04-02 09:34:02 +0200

    add sitances for cells

---
+++ CityPlanner/CityPlanner/Models/PeopleInfoView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityPlanner.Models
+{
+    public class PeopleInfoView
+    {
+        public int people {  get; set; }
+
+        public string value { get; set; }
+    }
+}
