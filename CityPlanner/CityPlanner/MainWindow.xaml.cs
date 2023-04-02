283521a89191fd5fab9862c6c1cb24bc82a787a7
kwimbik <kwimbiks@gmail.com>
2023-04-02 06:50:15 +0200

    add serivce btn

--- CityPlanner/CityPlanner/MainWindow.xaml.cs
+++ CityPlanner/CityPlanner/MainWindow.xaml.cs
@@ -231,6 +231,12 @@ namespace CityPlanner
             });
         }
 
+        private void AddService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
         #endregion
     }
 }
